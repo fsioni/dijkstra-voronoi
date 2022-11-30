ca1236f0b1fa5f4b92e4c43f3adbf827b7767bc8
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2022-11-30 16:26:29 +0100

    Add Graphe main test

---
+++ graphe_tests.cpp
@@ -0,0 +1,15 @@
+#include "Graphe.h"
+
+int main(){
+    Graphe g1;
+    g1.afficher();
+    /*
+    g1.testVoisins(0);
+    g1.testVoisins(1);
+    g1.testVoisins(6);
+    g1.testVoisins(7);
+    g1.testVoisins(11);
+    g1.testVoisins(17);
+    g1.testVoisins(18);
+    g1.testVoisins(23);*/
+}
